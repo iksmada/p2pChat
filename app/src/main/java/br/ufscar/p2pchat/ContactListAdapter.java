1770aa66f8651ee6b32408338cdf670b68e9ee1b
Raphael Adamski <raphaeladamski@hotmail.com>
2017-07-18 21:35:58 -0300

    create ContactListAdapter #10

---
+++ app/src/main/java/br/ufscar/p2pchat/ContactListAdapter.java
@@ -0,0 +1,101 @@
+package br.ufscar.p2pchat;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by adamski on 18/07/2017.
+ */
+
+class Contact{
+    private String name;
+    private String IP;
+
+    Contact(String name,String IP){
+        this.name = name;
+        this.IP = IP;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIP() {
+        return IP;
+    }
+}
+
+class ViewHolder {
+
+    final TextView tvName;
+    final TextView tvIp;
+
+    public ViewHolder(View view) {
+        tvName = (TextView) view.findViewById(R.id.name);
+        tvIp = (TextView) view.findViewById(R.id.ip);
+    }
+
+}
+
+public class ContactListAdapter extends BaseAdapter {
+
+    private final Context mContext;
+    private ArrayList<Contact> mData = new ArrayList<Contact>();
+    private LayoutInflater mInflater;
+
+    public ContactListAdapter(Context context, ArrayList<Contact> contacts) {
+        super();
+        mData = contacts;
+        mContext = context;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void addItem(final Contact item) {
+        mData.add(item);
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Contact getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position,
+                        View convertView, ViewGroup parent) {
+
+        View view;
+        ViewHolder holder;
+
+        if( convertView == null) {
+            view = mInflater.inflate(R.layout.contact_row, parent, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        } else {
+            view = convertView;
+            holder = (ViewHolder) view.getTag();
+        }
+        Contact contact = (Contact) getItem(position);
+        holder.tvName.setText(contact.getName());
+        holder.tvIp.setText(contact.getIP());
+
+        return view;
+    }
+}
