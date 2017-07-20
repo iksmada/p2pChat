9ced69a65520769041c397f78b0ff1390834ff13
brunowilliamsap <brunowilliamsap@gmail.com>
2017-07-19 21:46:41 -0300

    mensagem

--- app/src/main/java/br/ufscar/p2pchat/MainActivity.java
+++ app/src/main/java/br/ufscar/p2pchat/MainActivity.java
@@ -1,5 +1,6 @@
 package br.ufscar.p2pchat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,15 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final Intent intent = new Intent(this, SendMessageActivity.class);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //COLCOAR SUA AÇÂO AQUI
+                startActivity(intent);
             }
         });
+    }
+
+    public void startMessagesActivity(View v){
+        //clicaram em mensagens, devo abrir a activity de mensagens
     }
 
     @Override
---
+++ app/src/main/java/br/ufscar/p2pchat/SendMessageActivity.java
@@ -0,0 +1,13 @@
+package br.ufscar.p2pchat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SendMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_message);
+    }
+}
